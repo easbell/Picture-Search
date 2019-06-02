011c76fd4ee31f2dc6cb6db26c36c0a1fc21298d
Elizabeth Asbell <e.asbell8@gmail.com>
2019-06-02 17:14:03 -0600

    add path

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// server.js
 var express = require('express');
+const path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
