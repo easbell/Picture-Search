9f20a2d7b83cfc581af17bd6f60568fe690c7a6e
Elizabeth Asbell <e.asbell8@gmail.com>
2019-06-03 10:26:13 -0600

    Remove unsplash package.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Unsplash from 'unsplash-js';
 
 Vue.config.productionTip = false;
-
-export const unsplash = new Unsplash({
-  applicationId: process.env.VUE_APP_ACCESS_KEY,
-  secret: process.env.VUE_APP_SECRET_KEY
-});
 
 new Vue({
   render: h => h(App),
