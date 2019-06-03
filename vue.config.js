9dd1d535aa23ea5a88639ed19eb46c54940a858e
Elizabeth Asbell <e.asbell8@gmail.com>
2019-06-02 18:23:44 -0600

    keys

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    VUE_APP_ACCESS_KEY: JSON.stringify(process.env.VUE_APP_ACCESS_KEY),
-    VUE_APP_SECRET_KEY: JSON.stringify(process.env.VUE_APP_SECRET_KEY)
-})
